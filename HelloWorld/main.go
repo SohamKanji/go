e19cc5fb0d5ae922bc6795d8f72884712ff5cf05
Soham Kanji <soham.kanji@schrodinger.com>
2024-05-11 04:53:19 +0530

    Created Hello World Program

---
+++ HelloWorld/main.go
@@ -0,0 +1,7 @@
+package main // Name of the package determines what type of package we are creating. In this case, it's an executable package.
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
