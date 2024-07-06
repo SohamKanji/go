1ad80962806b9487984e662cca2f6bdc9245f51d
Soham Kanji <sohamkanji@Sohams-MacBook-Air.local>
2024-07-06 09:18:06 +0530

    Learned about channels

---
+++ Channels/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	// fmt.Println(<-c) // This will wait for the first response from the channel and then finish execution
+
+	for l := range c {
+		// time.Sleep(5 * time.Second) // this will make main routine to wait for 5 seconds before calling the go routine again, thus blocking the other go routines
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up!")
+	c <- link
+}
